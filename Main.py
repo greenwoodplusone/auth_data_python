82f9f567fdc7dbcb71fd0b69250a7eff4a7399f8
greenwoodplusone <greenwoodplusone@gmail.com>
2021-05-21 06:35:04 +0500

    add menu

--- Main.py
+++ Main.py
@@ -1,12 +1,17 @@
+from Menu import *
+
 # проверки на аторизованность пользователя и хранение его ID
-userAuth = False;
-authAdmin = False;
-userID = 0;
+userAuth = False
+authAdmin = False
+userID = 0
 
 # количество попыток вода данных
-counterMax = 3;
+counterMax = 3
 
 # указатель на счетчик пустых ячеек после удаления пользователя
 # int* ptrCountDeleteUser = nullptr;
 
 # bool valid;
+
+while True:
+    Menu.menu(True, False, False, Menu.menuNotAuthData, Menu.menuAuthData, Menu.menuAdminData, 1)
